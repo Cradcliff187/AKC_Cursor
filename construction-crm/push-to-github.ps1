267d621b7ec65b198a0637aa20f91e185237c2ff
Cradcliff187 <chris.l.radcliff@gmail.com>
2025-03-09 01:03:33 -0500

    Remove sensitive file

--- construction-crm/push-to-github.ps1
+++
@@ -1,5 +0,0 @@
-# This file should contain commands to push to GitHub
-# Make sure to use authentication methods that don't expose tokens
-# Example: Use SSH keys or credential manager instead
-
-# git push origin main 
